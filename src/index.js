2c9751c86dc982cdffd5f3da0ada9657c3fb0547
Jim Manton <jrman@risebroadband.net>
2019-03-13 13:24:33 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App';
+import './index.css';
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+
+alert(223);
+ReactDOM.render(
+  <App />,
+  document.getElementById('rootx')
+);
+
+//ReactDOM.render(<h1>Hello World</h1>, document.getElementById('rootx'));
